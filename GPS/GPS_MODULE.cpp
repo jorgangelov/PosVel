7c5bd59fa78d215bf0dda33cebbf4a6c1906c9a6
jorgangelov <jorgangelov@gmail.com>
2016-10-12 13:40:40 +0200

    - contd.: Parsing of Data

--- GPS/GPS_MODULE.cpp
+++ GPS/GPS_MODULE.cpp
@@ -3,8 +3,8 @@
 
 void cNEO::begin()
 {
-    Uart.begin();
-    Serial.println("GPS Module Started!");
+  Uart.begin();
+  Serial.println("GPS Module Started!");
 }
 
 
@@ -12,22 +12,64 @@ void cNEO::update()
 {
   if (Uart.available())
   {
-      char line[200];
-      uint8_t num_bytes = Uart.getData(line);
-      line[num_bytes] = '\0';
-      
-      char* token;
-      token = strtok(line,",");
-      while(token != NULL)
+    // Read Buffer
+    char line[200];
+    uint8_t num_bytes = Uart.getData(line);
+    line[num_bytes] = '\0';
+
+
+    bool found_beginning = false;
+    char* token;
+
+    // Find First Entrie
+    token = strtok(line, ", \n");
+
+    // Check if sth is found and if it matches the Header
+    if ( token != NULL && (strcmp(token, "&GPGGA") == 0) )
+    {
+      found_beginning = true;
+    }
+
+    // Do while sth is found and it doesnt match the header
+    while (token != NULL && !found_beginning)
+    {
+      token = strtok(NULL, ", \n");
+      if (strcmp(token, "&GPGGA") == 0)
       {
-      if (*token == "$GPGGA")
-        {
-            
-        }
-      token = strtok(NULL,",");
+        found_beginning = true;
       }
- 
-      
+
+    }
+
+    // Found the Header
+    if (found_beginning)
+    {
+      char* chrpt_array[15];
+      int i = 0;
+      // Do while sth is found and not more than 15 Entries are found
+      while (token != NULL && i < 15)
+      {
+        token = strtok(NULL, ",");
+        chrpt_array[i] = token;
+        i++;
+
+      }
+
+      // Finished
+      char out_line[50];
+      sprintf(out_line, "Found %i Entries", i - 1);
+      Serial.println(out_line);
+      // Convert Entries to numbers
+      float numbers[15];
+      for (int j = 0; j < i - 1; j++)
+      {
+        numbers[j] = atof( chrpt_array[j] );
+        Serial.print(numbers[j]);
+        Serial.print(" ");
+      }
+      Serial.println("");
+
+    }
 
   }
 
